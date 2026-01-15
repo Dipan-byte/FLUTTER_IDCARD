6508c52677db3c55f586ca4b0f6a0accf17ca052
Dipan Mallick <dipanmallick39@gmail.com>
2026-01-15 20:01:00 +0530

    done as a stateless widget

--- idcard/lib/main.dart
+++ idcard/lib/main.dart
@@ -10,8 +10,14 @@ class IdCard extends StatelessWidget{
         return Scaffold(
           backgroundColor: Colors.blue[700],
           appBar: AppBar(
-            title: Text('IDCard') ,
+            title: Text(
+              'ID CARD' ,
+            style: TextStyle(
+              color: Colors.white70,
+            ),
+            ) ,
             backgroundColor: Colors.blue[500],
+            
             centerTitle: true,
             elevation: 0.0,
           ),
@@ -21,10 +27,20 @@ class IdCard extends StatelessWidget{
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
+                Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/KanekiAnime2.webp'),
+                    radius: 90.0,
+                  ),
+                ),
+                Divider(
+                  height: 60.0,
+                  color: Colors.white70,
+                ),
                 Text(
                   'NAME:',
                   style: TextStyle(
-                    color: Colors.black87 ,
+                    color: Colors.white70 ,
                     letterSpacing: 2.0 ,
                     fontWeight: FontWeight.bold,
                     fontSize: 15.0
@@ -32,7 +48,7 @@ class IdCard extends StatelessWidget{
                 ),
                 SizedBox(height: 10.0,),
                 Text(
-                  'DIPAN',
+                  'KEN KANEKI',
                   style: TextStyle(
                     color: Colors.white70 ,
                     letterSpacing: 2.0 ,
@@ -45,7 +61,7 @@ class IdCard extends StatelessWidget{
                 Text(
                   'CURRENT-LEVEL',
                   style: TextStyle(
-                    color: Colors.black87 ,
+                    color: Colors.white ,
                     letterSpacing: 2.0 ,
                     fontWeight: FontWeight.bold,
                     fontSize: 15.0
@@ -71,12 +87,14 @@ class IdCard extends StatelessWidget{
                     ),
                     SizedBox(width: 10.0,),
                     Text(
-                      'dipanmallick39@gmail.com' ,
+                      'kanekiken@gmail.com' ,
                       style: TextStyle(
                         color: Colors.white70 ,
                         fontSize: 20.0 ,
+                        letterSpacing: 2.0,
                       ),
-                    )
+                    ),
+                    
                   ]
                 )
             ]
